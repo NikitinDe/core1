0cb9aaa3788d6d0bbc5d76c26653dfeb81316b69
Nikitin De <456_1996@mail.ru>
2024-01-21 18:21:40 +0300

    Test

---
+++ src/task/methods/Methods.java
@@ -0,0 +1,166 @@
+package task.methods;
+
+public class Methods {
+    public static void main(String[] args) {
+        // Пример вызова метода
+        String str = "АААААа";
+        String string1 = "5";
+        int[] massiv = {2, 2, 2, 4};
+        boolean boolean2 = true;
+        boolean boolean1 = true;
+        // Ожидаемый результат hello System.out.println(string);
+        // Вызовы ваших методов
+        int a = 350;
+        int max = 1000;
+        int b = 2;
+        //   System.out.println(booleanGo(string,a));
+        System.out.println(wallet(a,max));
+        // System.out.println(Arrays.toString(sumArray(massiv)));
+        //System.out.println(vozrast(a)+ " Месяцев");
+        //System.out.println(arrays(massiv));
+    }
+
+    // Метод принимающий строку и преобразующий ее в нижний регистр
+    public static String getString(String example) {
+        return example.toLowerCase();
+    }
+
+// Реализуйте функциональность согласно описанию
+
+    // TODO: Создайте метод который вернет сумму двух чисел
+    public static int sumInteger(int a, int b) {
+        return a + b;
+    }
+
+    // TODO: Создайте метод который вернет сумму двух переданных строк
+    public static String sumString(String string, String string1) {
+        return string + string1;
+    }
+
+    // TODO: создайте метод который примет строку внутри которой передается число , а также принимает число . Метод должен суммировать числа
+//  обернуть их в строку и вернуть результат строкой
+    public static String sumToString(String string, String string1) {
+        int pervoe = Integer.parseInt(string);
+        int vtoroe = Integer.parseInt(string1);
+        return String.valueOf(pervoe + vtoroe);
+    }
+
+    // TODO: Создайте метод который принимает строку , число . Внутри строки передается число . Проверить , что если число внутри строки больше чем
+//  число которое передается как int , то пусть метод вернет false , а если меньше , то true
+    public static boolean booleanGo(String string, int a) {
+        int one = Integer.parseInt(string);
+        boolean b = one < a;
+        return b;
+    }
+
+    // TODO: Создайте метод который принимает 2 булевые переменные . Если обе true , верни true , если хоть одна из них true верни true ,
+//  если обе false верни false
+    public static boolean exeption(boolean boolean1, boolean boolean2) {
+        if (boolean1 == true || boolean2 == true) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    // TODO: Создайте метод который принимает число и вернет true если оно четное , и false если оно не четное
+    public static boolean chetnoe(int a) {
+        if (a % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    // TODO: Создайте метод который проверят числа в массиве , если все они четные верни "Массив четных чисел" , если есть и четные и не
+//  четные верни "Массив четных и не четных чисел" и если в массиве только нечетные числа верни "Массив не четных чисел"
+    public static String array(int[] massiv) {
+        boolean god = true;
+        boolean goodbay = true;
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] % 2 == 0) {
+                god = false;
+            } else {
+                goodbay = false;
+            }
+
+        }
+        if (goodbay) {
+            return "Массив четных чисел";
+        } else if (god) {
+            return "Массив  не четных чисел";
+        } else {
+
+        }
+        return "Массив четных и не четных чисел";
+    }
+
+    // TODO: Создайте метод который принимает массив чисел и возвращает новый массив чисел . Внутри метода пройдитесь по элементам
+//  массива который подается в аргументы  и сложите все элементы в новый массив и верните его
+    public static int[] arrayNumbers(int[] massiv) {
+        int[] arrays = new int[massiv.length];
+        for (int i = 0; i < massiv.length; i++) {
+            massiv[i] = arrays[i];
+        }
+        return arrays;
+    }
+
+    // TODO: Создайте метод который принимает массив чисел и возвращает число . Внутри метода пройдитесь по элементам массива который подается в
+//  аргументы сложите все числа который переданы в массиве и верните сумму всех чисел
+    public static int[] sumArray(int[] massiv) {
+        int sum = 0;
+        for (int num : massiv) {
+            sum += num;
+        }
+        return new int[]{sum};
+
+    }
+
+
+    // TODO: Создайте методы в которые можно будет передавать свой возраст , а возвращаться сколько полных месяцев в этом возрасте ,
+//  тоесть если человек прожил 10 лет , это 120 месяцев так же сделайте перевод возраста в минуты и метод который расчитает возраст в часах
+    public static int ageInMonths(int age) {
+        return age * 12;
+    }
+
+    public static long minutes(int age) {
+        return age * 365 * 24 * 60;
+    }
+
+    public static long hours(int age) {
+        return age * 365 * 24;
+    }
+
+    // TODO: Создайте метод которому передаю имена , а он проверяет одинаковые ли они , причем не зависимо от регистра
+    public static String name(String string, String string1) {
+        return String.valueOf(string.equalsIgnoreCase(string1));
+    }
+
+    // TODO: Создайте метод которому можно передать массив элементов , а он вернет последний элемент
+    public static int arrays(int[] massiv) {
+        return massiv[massiv.length - 1];
+    }
+
+    // TODO: Создайте метод счетчик букв А . Подается строка и метод посчитает сколько букв А в строке
+    public static int schetcik(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'А' || str.charAt(i) == 'а') {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    // TODO: Создайте метод кошелек , у него есть началная сумма и можно вычитать пока деньги не кончатся или сумма не будет слишком велика
+    public static int wallet(int a, int max) {
+        while (a > 0 && a <= max) {
+            a--;
+        }
+        return a;
+    }
+}
---
+++ src/task/oop/OOP.java
@@ -0,0 +1,222 @@
+package task.oop;
+
+public class OOP {
+    public static void main(String[] args) {
+       // Parent.Manager man =new Parent.Manager("Коля",36,"продаж.");
+       // man.job();
+      // Parent.Child1 den = new Parent.Child1();
+       //den.sum(10,5);
+
+
+    }
+
+// Реализуйте функциональность согласно описанию
+
+    // TODO: Создайте класс Parent(Родитель) и создайте для него 5 методов.
+    //  1. Метод, принимающий 2 числа и возвращающий их сумму.
+    //  2. Метод, принимающий строку, в которой передано число, и число, а возвращающий сумму в виде числа.
+    //  3. Метод, принимающий строку и возвращающий ее.
+    //  4. Метод, не принимающий и не возвращающий ничего, лишь выводящий на экран "I am Parent".
+    //  5. Метод, возвращающий конкатенацию (сумму двух строк), принимающий 2 строки.
+    public static class Parent {
+        public int sum(int a, int b) {
+            return a + b;
+        }
+
+        public int summ(String a, int b) {
+            int chislo = Integer.parseInt(a);
+            return chislo + b;
+        }
+
+        public String strings(String str) {
+            return str;
+        }
+
+        public void message() {
+            System.out.println("I am Parent");
+        }
+
+        public String summa(String str, String str1) {
+            return str + str1;
+        }
+
+        // TODO: Создайте класс Child1 и переопределите все 5 методов.
+        //  1. Первый метод переопределите так, что вместо суммы первый метод перемножает числа, а не складывает.
+        //  2. Второй метод переопределите, чтобы получившееся число после суммирования умножалось на 2.
+        //  3. Третий метод переопределите, чтобы возвращал только первую букву из строки.
+        //  4. Четвертый метод выводит на экран "I am Child1".
+        //  5. Пятый метод после слияния двух строк должен обрезать строку и вернуть только ее половину.
+        public  static class Child1 extends Parent {
+            public int sum(int a, int b) {
+                return a * b;
+            }
+
+            @Override
+            public int summ(String a, int b) {
+                int chislo = Integer.parseInt(a);
+                return (chislo + b) * 2;
+            }
+
+            public String strings(String str) {
+                return str.substring(0, 1);
+            }
+
+            @Override
+            public void message() {
+                System.out.println("I am Child1");
+            }
+
+            @Override
+            public String summa(String str, String str1) {
+                return (str1 + str).substring((str1 + str).length() / 2);
+            }
+        }
+
+        // TODO: Создайте класс Child2 и перегрузите методы.
+        //  1. Первый метод примет 3 числа и сложит их.
+        //  2. Второй метод примет 3 строки, внутри которых лежат числа, и вернет их сумму.
+        //  3. Третий метод примет две строки и вернет их конкатенацию.
+        //  4. Четвертый метод примет аргумент строку и выведет на экран "I am Child2" + переданная строка.
+        //  5. Пятый метод принимает 3 строки и возвращает их конкатенацию.
+        public  static class Child2 extends Parent {
+            public int sum(int a, int b, int c) {
+                return a + b + c;
+            }
+
+            public int summ(String str1, String str, String str2) {
+                int chislo = Integer.parseInt(str);
+                int chislo1 = Integer.parseInt(str1);
+                int chislo2 = Integer.parseInt(str2);
+                return chislo + chislo1 + chislo2;
+            }
+
+            public String strings(String str, String str2) {
+                return str + str2;
+            }
+
+
+            public void message(String str) {
+                System.out.println(str + "I am Child2");
+
+            }
+
+
+            public String summa(String str, String str1, String str2) {
+                return str1 + str + str2;
+            }
+        }
+
+        // TODO: Создайте абстрактный класс "Фигура", в котором определены общие свойства и методы для всех геометрических фигур
+        //  (например, метод для расчета площади).
+        //  От него унаследуйте классы "Круг", "Прямоугольник" и "Треугольник", переопределив специфичные методы для каждой фигуры.
+        public static abstract class figure {
+            public abstract double square();
+        }
+
+        public static class circule extends figure {
+            private double rad;
+
+            public circule(double rad) {
+                this.rad = rad;
+            }
+
+            @Override
+            public double square() {
+                return rad * Math.PI * rad;
+            }
+
+            public class rectangle extends figure {
+                private double s;
+                private double v;
+
+                public rectangle(double s) {
+                    this.s = s;
+                    this.v = v;
+                }
+
+                @Override
+                public double square() {
+                    return s * v;
+                }
+            }
+
+            public class triangle extends figure {
+                private double s;
+                private double v;
+                private double d;
+
+                public triangle(double s, double v, double d) {
+                    this.s = s;
+                    this.v = v;
+                    this.d = d;
+                }
+
+                @Override
+                public double square() {
+                    return Math.abs((s - v) * (v - s) - (s - d) * (d - s));
+                }
+            }
+        }
+
+        // TODO: Создайте абстрактный класс "Сотрудник", в котором определены общие характеристики для всех сотрудников (например, имя, возраст).
+        //  Создайте подклассы для различных типов сотрудников (например, "Менеджер", "Работник склада"),
+        //  в которых можно описать специфичные методы и свойства.
+        public  static abstract class Employee {
+            private String name;
+            private int age;
+
+
+            public Employee(String name, int age) {
+                this.name = name;
+                this.age = age;
+
+            }
+
+            public String getName() {
+                return name;
+
+            }
+
+            public int getAge() {
+                return age;
+            }
+
+
+            public abstract void job();
+        }
+
+        public static   class  Manager extends Employee{
+            private String departement;
+            public Manager(String name, int age, String departement) {
+                super(name, age);
+                this.departement = departement;
+            }
+
+            public String getDepartement() {
+                return departement;
+            }
+
+
+            @Override
+            public void job() {
+                System.out.println("Менджер: "+getName()+ ", в возраст лет "+ getAge()+ " работает в отделе " +getDepartement());
+            }
+        }
+        public  static  class Worker extends Employee{
+private String classification;
+            public Worker(String name, int age,String classification) {
+                super(name, age);
+                this.classification =classification;
+            }
+
+            public String getClassification() {
+                return classification;
+            }
+
+            @Override
+            public void job() {
+                System.out.println("Работник склада "+ getName()+",в возраст "+ getAge()+ "в"+getClassification());
+            }
+        }
+    }
+}
